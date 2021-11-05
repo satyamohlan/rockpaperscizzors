baa1e0bf658f5d9a36d3f1395a8a42dd1e236ff6
Satyam Ohlan <satyam.ohlan1998@gmail.com>
2021-11-06 00:19:30 +0530

    ok

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 const port = 3000;
 app.set('view engine', 'ejs');
-app.listen(port, () => console.log(`listening on port ${port}`));
+server.listen(port, () => console.log(`listening on port ${port}`));
 app.use(
   express.urlencoded({
     extended: false,
   })
 );
 app.use('/css', express.static('views/css'));
+app.use('/images', express.static('views/images'));
+
 app.get('/', (req, res) => res.render('index'));
